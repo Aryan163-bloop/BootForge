48246b27b2b12db6bf63179158a514731e8b09a4
Aryan163-bloop <githubarya3579@gmail.com>
2026-07-14 18:37:14 +0530

    Installation

--- js/script.js
+++ js/script.js
@@ -21,3 +21,46 @@ async function loadComponent(id, file) {
 loadComponent("navbar","components/navbar.html");
 
 loadComponent("footer","components/footer.html");
+// ==========================================
+// COPY CODE BUTTON
+// ==========================================
+
+const copyButtons = document.querySelectorAll(".copy-btn");
+
+copyButtons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        const targetId = button.dataset.copyTarget;
+
+        const code = document
+            .getElementById(targetId)
+            .innerText;
+
+        navigator.clipboard.writeText(code);
+
+        button.innerHTML = `
+            <i class="bi bi-check2"></i>
+            <span>Copied!</span>
+        `;
+
+        button.classList.remove("btn-primary");
+
+        button.classList.add("btn-success");
+
+        setTimeout(() => {
+
+            button.innerHTML = `
+                <i class="bi bi-copy"></i>
+                <span>Copy</span>
+            `;
+
+            button.classList.remove("btn-success");
+
+            button.classList.add("btn-primary");
+
+        }, 2000);
+
+    });
+
+});
